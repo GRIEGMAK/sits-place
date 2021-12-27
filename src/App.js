ba9f210fd3fa6e72ae0f1b207f0968afe53f469d
GRIEGMAK <g.m.ak@yandex.ru>
2021-12-27 12:28:20 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import LeftBar from './components/LeftBar.jsx';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = () => {
+  return (    
+    <Router>
+      <div><Link to="/client/">API</Link></div>
+    <Switch>
+    <Route path="/client">
+      <div>Client</div>
+    </Route>
+    <Route path="/">
+      <LeftBar />
+    </Route>
+  </Switch>
+  </Router>
   );
 }
 
---
+++ src/components/LeftBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link, NavLink} from "react-router-dom";
+
+const LeftBar = () => {
+    console.log("bdckndld")
+    return(
+    <div>
+        <div>
+            <Link to="/client/">API</Link>
+            <div>
+
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default LeftBar;
